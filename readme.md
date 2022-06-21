a50e15b9ba0419fe2b2132a48d6c427665d15ec1
NotJansel <73423410+NotJansel@users.noreply.github.com>
2022-06-22 00:16:31 +0200

    base source

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Palladium
+A complete rewrite of my old [datasync](https://github.com/tornrpg/datasync) plugin. Development begins when the last update for the datasync plugin is pushed.
---
+++ src/main/java/de/notjansel/palladium/Palladium.java
@@ -0,0 +1,17 @@
+package de.notjansel.palladium;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Palladium extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
