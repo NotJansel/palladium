07c1eddf8d12cf95c81d087df31b27a51e32a3f4
NotJansel <73423410+NotJansel@users.noreply.github.com>
2022-06-22 00:24:00 +0200

    refactor base to kotlin

--- src/main/kotlin/de/notjansel/palladium/Palladium.kt
+++ src/main/kotlin/de/notjansel/palladium/Palladium.kt
@@ -1,17 +1,14 @@
-package de.notjansel.palladium;
+package de.notjansel.palladium
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit
+import org.bukkit.plugin.java.JavaPlugin
 
-public final class Palladium extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
+class Palladium : JavaPlugin() {
+    override fun onEnable() {
+        Bukkit.getConsoleSender().sendRichMessage("<red>Palladium is initializing")
     }
 
-    @Override
-    public void onDisable() {
+    override fun onDisable() {
         // Plugin shutdown logic
     }
 }
